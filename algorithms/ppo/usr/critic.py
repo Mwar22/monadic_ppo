c48644b0c4a44c51ba0648745a93daeacf4308a2
Mwar22 <lucasdejesusphysic@gmail.com>
2026-06-14 13:52:12 -0300

    changed most of layers to float16, so they use most of tensorcores. last layers are float32 to avoid problems with logs used in ppo

--- algorithms/ppo/usr/critic.py
+++ algorithms/ppo/usr/critic.py
@@ -5,10 +5,10 @@
 # Author: Lucas de Jesus  (lucasdejesusphysic@gmail.com)
 # -----
 # Last Modified: 25/05/2026 10:52:34
-# Modified By: Lucas de Jesus 
+# Modified By: Lucas de Jesus
 # -----
 # Copyright (c) 2026
-# 
+#
 # This file is subject to the terms and conditions defined in
 # the 'LICENSE.txt' file found in the root of this source tree.
 # Please read LICENSE.txt for full copyright and licensing details.
@@ -29,27 +29,59 @@ hidden_init = nnx.initializers.orthogonal(jnp.sqrt(2))
 critic_init = nnx.initializers.orthogonal(0.01)
 activation = jax.nn.leaky_relu
 
-smooth_bound = lambda x: MIN_ALPHA_BETA + (MAX_ALPHA_BETA - MIN_ALPHA_BETA) * jax.nn.sigmoid(x)
+smooth_bound = lambda x: (
+    MIN_ALPHA_BETA + (MAX_ALPHA_BETA - MIN_ALPHA_BETA) * jax.nn.sigmoid(x)
+)
+
 
 class Critic(nnx.Module):
-    def __init__(self, obs_size: int,  rngs: nnx.Rngs):
+    def __init__(self, obs_size: int, rngs: nnx.Rngs):
         self.obs_size = obs_size
-    
-        self.linear1 = nnx.Linear(obs_size, 256, kernel_init=hidden_init, rngs=rngs)
-        self.linear2 = nnx.Linear(256, 256, kernel_init=hidden_init, rngs=rngs)
-        self.linear3 = nnx.Linear(256, 64, kernel_init=hidden_init, rngs=rngs)
 
-        self.out_layer = nnx.Linear(64, 1, kernel_init=critic_init, rngs=rngs)
+        self.linear1 = nnx.Linear(
+            obs_size,
+            256,
+            kernel_init=hidden_init,
+            rngs=rngs,
+            dtype=jnp.float16,
+            param_dtype=jnp.float32,
+        )
+        self.linear2 = nnx.Linear(
+            256,
+            256,
+            kernel_init=hidden_init,
+            rngs=rngs,
+            dtype=jnp.float16,
+            param_dtype=jnp.float32,
+        )
+        self.linear3 = nnx.Linear(
+            256,
+            64,
+            kernel_init=hidden_init,
+            rngs=rngs,
+            dtype=jnp.float16,
+            param_dtype=jnp.float32,
+        )
 
-    def __call__(self, obs:jax.Array)->jax.Array:
+        self.out_layer = nnx.Linear(
+            64,
+            1,
+            kernel_init=critic_init,
+            rngs=rngs,
+            dtype=jnp.float32,
+            param_dtype=jnp.float32,
+        )
+
+    def __call__(self, obs: jax.Array) -> jax.Array:
         x1 = self.linear1(obs)
         x1 = activation(x1)
-        
+
         x2 = self.linear2(x1)
         x2 = activation(x2)
-        
+
         x3 = self.linear3(x2)
         x3 = activation(x3)
 
         out = self.out_layer(x3)
         return out.squeeze(-1)
+
